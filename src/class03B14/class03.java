1152aca23f271e591d2cb158b49a156daf70dc5e
isabellavpsilva <isabellavpsilva@gmail.com>
2022-09-27 19:32:39 -0400

    simple basic project

---
+++ src/class03B14/class03.java
@@ -0,0 +1,14 @@
+package class03B14;
+
+public class class03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Isabella pacheco");
+		
+
+	}
+
+}
